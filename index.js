a33659d3b3a18da7221bbd8eabfd1dd5a2a2dfb2
nagasai7772 <nagunagasai@gmail.com>
2024-10-24 16:13:33 +0530

    if else

---
+++ index.js
@@ -0,0 +1,104 @@
+// task:
+// ------
+// 1.Traffic Light System
+// Simulate a traffic light system:
+
+// If the light is red, log "Stop."
+// If the light is yellow, log "Get ready to move."
+// If the light is green, log "Go."
+// If the input doesn't match any color, log "Invalid input."
+
+// 2.Weather Description (Temperature Descriptions)
+// Write a program that describes the temperature:
+
+// If the temperature is exactly 0, log "It's freezing cold."
+// If the temperature is exactly 15, log "It's a cool day."
+// If the temperature is exactly 25, log "It's a pleasant day."
+// If the temperature doesn't match any of these, log "Unknown weather."
+
+// 3.: Day of the Week
+
+// Create a program that takes input of a number representing the day of the week (1-7):
+
+// If the number is 1, log "Today is Monday."
+// If the number is 2, log "Today is Tuesday."
+// If the number is 3, log "Today is Wednesday."
+// If the number is 4, log "Today is Thursday."
+// If the number is 5, log "Today is Friday."
+// If the number is 6, log "Today is Saturday."
+// If the number is 7, log "Today is Sunday."
+// If the input is not between 1 and 7, log "Invalid day number."
+
+// var traffic="red";
+// if(traffic=="red")
+// {
+//     console.log("stop");
+// }
+
+// var traffic= "Red";
+// if(traffic=="Red")
+// {
+//     console.log(traffic,':',"Stop");
+// }
+// else if(traffic=="Yellow"){
+//     console.log(traffic,':',"Get Ready to Move");
+// }
+// else if(traffic=="Green")
+// {
+//     console.log(traffic,':',"Go")
+// }
+// else{
+//     console.log("Invalid input");
+// }
+// const readline = require('readline').createInterface({
+//     input: process.stdin,
+//     output: process.stdout
+//   });
+
+//   readline.question('Enter value: ', (input) => {
+//     const traffic = parseInt(input);
+
+//     if (traffic == 1) {
+//       console.log("It's a Red");
+//     }
+
+//     readline.close();
+//   });
+
+// var temp = 2;
+
+// if (temp == 0) 
+//     {
+//   console.log("Its a Freezing cold", temp);
+//     }
+//   else if (temp == 1) {
+//     console.log("It's a Cool Day",temp);
+//   }
+//  else if (temp == 2) {
+//     console.log("It's a Pleasant Day",temp);
+//   } 
+//   else {
+//     console.log("Unknown Weather");
+//   }
+
+// var day = 2;
+
+// if (day == 1) {
+//   console.log(day, "Monday");
+// } else if (day == 2) {
+//   console.log(day, "Tuesday");
+// } else if (day == 3) {
+//   console.log(day, "wednesday");
+// } else if (day == 4) {
+//   console.log(day, "Thursday");
+// } else if (day == 5) {
+//   console.log(day, "Friday");
+// } else if (day == 6) {
+//   console.log(day, "Saturday");
+// } else if (day == 7) {
+//   console.log(day, "Sunday");
+// }
+// else
+// {
+//     console.log("Invalid input");
+// }
